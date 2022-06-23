b0a16c364e80cf7d44e2a6ec358b0a8d77f62acf
Indu-412 <chitra231b@gmail.com>
2022-06-23 12:13:56 +0530

    Added QuotesController

--- src/Services/Quotes/Quotes.API/Models/QuotesMaster.cs
+++ src/Services/Quotes/Quotes.API/Models/QuotesMaster.cs
@@ -1,12 +1,27 @@
 #nullable disable
+using System.ComponentModel.DataAnnotations;
 namespace Quotes.Models;
-public class Quotes
+public class QuotesMaster
 {
     public Guid Id { get; set; }
-    
-    public int businessValue { get; set; }
-    public int propertyValue { get; set; }
-    public string propertyType { get; set; }
-    public string quotes { get; set; }
+
+    public int MinBusinessValue { get; set; }
+    public int MaxBusinessValue { get; set; }
+    public int BusinessValue { get
+    {return this.BusinessValue;}
+     set{
+        if(value>MinBusinessValue && value<MaxBusinessValue)
+        this.BusinessValue=value;
+    } }
+    public int MinPropertyValue { get; set; }
+    public int MaxPropertyValue { get; set; }
+    public int PropertyValue { get
+    {return this.PropertyValue;}
+     set{
+        if(value>MinPropertyValue && value<MaxPropertyValue)
+        this.PropertyValue=value;
+    } } 
+    public string PropertyType { get; set; }
+    public string quoteValue { get; set; }
 
 }
