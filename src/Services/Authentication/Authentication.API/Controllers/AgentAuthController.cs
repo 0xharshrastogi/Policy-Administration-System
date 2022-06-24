856a167bd60ff0bc402f2fb9a91e8fcfa2ed3773
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-06-24 21:55:14 +0530

    AuthCookie Expire Automatically

--- src/Services/Authentication/Authentication.API/Controllers/AgentAuthController.cs
+++ src/Services/Authentication/Authentication.API/Controllers/AgentAuthController.cs
@@ -1,3 +1,5 @@
+using System.IdentityModel.Tokens.Jwt;
+
 using Authentication.DTO;
 using Authentication.Extensions;
 using Authentication.Models;
@@ -26,15 +28,35 @@ public class AuthController : ControllerBase
     [HttpPost("Signup")]
     public async Task<ActionResult> SignUpAsync(AgentCreateDTO agentCreate)
     {
-        if (!ModelState.IsValid)
-            return BadRequest();
+        if (!ModelState.IsValid) return BadRequest();
 
-        await _agentRepo.CreateAsync(_mapper.Map<Agent>(agentCreate));
+        var agent = _mapper.Map<Agent>(agentCreate);
+
+        if (false)
+            await _agentRepo.CreateAsync(agent);
+
+        var expires = DateTime
+            .UtcNow
+            .AddMinutes(1);
+
+        var token = Jwt.CreateToken(
+            keyValues: new Dictionary<string, string>
+            {
+                [nameof(agent.UserName)] = agent.UserName,
+                [nameof(agent.Name)] = agent.Name,
+                [nameof(agent.Id)] = agent.Id.ToString()
+            },
+            expires
+        );
+
+        Response
+            .Cookies
+            .Append("jwt_token", token, new() { Expires = expires });
 
         return Created(nameof(SignUpAsync), null);
     }
 
-    [HttpPost("Login")]
+    [HttpGet("Login")]
     public async Task<ActionResult> Login(AgentCredential credential)
     {
         if (!ModelState.IsValid) return BadRequest();
@@ -47,5 +69,25 @@ public class AuthController : ControllerBase
         var isCorrect = agentDetail.Compare(credential);
 
         return Ok(new { isCorrect });
+    }
+
+    [HttpGet("Validate")]
+    public IActionResult Validate()
+    {
+        var token = Request.Cookies["jwt_token"];
+        if (string.IsNullOrEmpty(token) || string.IsNullOrWhiteSpace(token))
+            return Unauthorized();
+
+        var jwtSecurityHandler = new JwtSecurityTokenHandler();
+        if (!jwtSecurityHandler.CanReadToken(token)) return BadRequest("Value is not a valid JWT token");
+
+        var securityToken = jwtSecurityHandler.ReadJwtToken(token);
+
+        if (DateTime.UtcNow > securityToken.ValidTo)
+        {
+
+        }
+
+        return Ok(new { securityToken.Claims, securityToken.ValidTo });
     }
 }
