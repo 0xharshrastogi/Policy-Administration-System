cd4b7c5ebe8cd7ee2b81e576fa67721de14f666e
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-06-28 13:48:57 +0530

    Signup route

--- src/Client/InsuretyPortal/src/app/input-text/input-text.component.ts
+++ src/Client/InsuretyPortal/src/app/input-text/input-text.component.ts
@@ -9,6 +9,7 @@ export class InputTextComponent implements OnInit {
   @Input() mode: 'light' | 'dark' = 'dark';
   @Input() type: 'text' | 'password' | 'email' = 'text';
   @Input() placeholder: string = '';
+  @Input() name: string = '';
 
   constructor() {}
 
