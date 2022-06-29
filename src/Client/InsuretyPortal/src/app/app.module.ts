b8244d7c878384ada4866a90fafec280362f9f3a
Monojit-saha333 <monojitsahajolly333@gmail.com>
2022-06-29 13:54:16 +0530

    Added customer route

--- src/Client/InsuretyPortal/src/app/app.module.ts
+++ src/Client/InsuretyPortal/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { PolicyDisplayComponent } from './policy-display/policy-display.componen
 import { ListPolicyComponent } from './list-policy/list-policy.component';
 import { CustomerViewComponent } from './pages/customer-view/customer-view.component';
 import { CustomerInputComponent } from './pages/customer-input/customer-input.component';
+import { CustomerBusinessComponent } from './customer-business/customer-business.component';
+import { ConsumerBusinessPropertyComponent } from './consumer-business-property/consumer-business-property.component';
 
 
 
@@ -30,7 +32,9 @@ import { CustomerInputComponent } from './pages/customer-input/customer-input.co
     PolicyDisplayComponent,
     ConsumersDisplayComponent,
     CustomerViewComponent,
-    CustomerInputComponent
+    CustomerInputComponent,
+    CustomerBusinessComponent,
+    ConsumerBusinessPropertyComponent
 
   ],
   imports: [CommonModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
