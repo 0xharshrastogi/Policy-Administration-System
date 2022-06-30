d4add5d1ff64714184251ff312abe5ffe0476ccf
Monojit-saha333 <monojitsahajolly333@gmail.com>
2022-06-30 19:24:29 +0530

    Customer Frontend edited

--- src/Client/InsuretyPortal/src/app/consumer-business-property/consumer-business-property.component.ts
+++ src/Client/InsuretyPortal/src/app/consumer-business-property/consumer-business-property.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-consumer-business-property',
   templateUrl: './consumer-business-property.component.html',
-  styleUrls: ['./consumer-business-property.component.css']
+  styleUrls: ['./consumer-business-property.component.css'],
 })
 export class ConsumerBusinessPropertyComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
