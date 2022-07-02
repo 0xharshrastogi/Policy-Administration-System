2aa1c530098100bb99df5798f2d1c940e72df439
Indu-412 <chitra231b@gmail.com>
2022-07-02 22:06:20 +0530

    Policy Tests Added

--- src/Services/Policy/Policy.UnitTests/UnitTest1.cs
+++ src/Services/Policy/Policy.UnitTests/UnitTest1.cs
@@ -1,7 +1,18 @@
+#nullable disable
+#pragma warning disable NUnit2021
+using Policy.Controllers;
+using Policy.Models;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 namespace Policy.UnitTests;
 
 public class Tests
 {
+
     [SetUp]
     public void Setup()
     {
@@ -12,4 +23,55 @@ public class Tests
     {
         Assert.Pass();
     }
+    private readonly PolicyController _controller;
+    [Test]
+    public void ViewPolicy_WhenCalled_ReturnsOkResult()
+    {
+        try
+        {
+            var g1 = new Guid("aa2868d9-fdbc-4dd5-3ea2-08da5aa9e271");
+            var offerItem = _controller.ViewPolicy(g1);
+            ObjectResult result2 = offerItem as ObjectResult;
+            Assert.AreEqual(200, result2.StatusCode);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    [Test]
+    public void ViewPolicy_UnknownGuidPassed_ReturnsNotFoundResult()
+    {
+        try
+        {
+            var g1 = Guid.NewGuid();
+            var offerItem = _controller.ViewPolicy(g1);
+            ObjectResult result2 = offerItem as ObjectResult;
+            Assert.AreEqual(404, result2.StatusCode);
+            Assert.Pass();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    [Test]
+    public void GetPoliciesByBusinessValue_WhenCalled_ReturnsOkResult()
+    {
+        try
+        {
+            int businessValue = 8;
+            var offerItem = _controller.GetPoliciesByBusinessValue(businessValue);
+            ObjectResult result2 = offerItem as ObjectResult;
+            Assert.AreEqual(200, result2.StatusCode);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
+
 }
--- src/Services/Quotes/Quotes.UnitTests/UnitTest1.cs
+++ src/Services/Quotes/Quotes.UnitTests/UnitTest1.cs
@@ -1,7 +1,12 @@
+#nullable disable
+using Quotes.API.Controllers;
+using Quotes.API.Models;
+
 namespace Quotes.UnitTests;
 
 public class Tests
 {
+    //private QuotesController _quotesController;
     [SetUp]
     public void Setup()
     {
@@ -12,4 +17,37 @@ public class Tests
     {
         Assert.Pass();
     }
+
+    /*[Test]
+    public void Get_WhenCalled_ReturnsOkResult()
+    {
+        try
+        {
+                int businessValue=5;
+                string PropertyType="Equipment";
+                int PropertyValue=3;
+            var offerItem = -_quotesController.GetQuotesAsync(businessValue,propertyValue,propertyType);
+            ObjectResult result2 = offerItem as ObjectResult;
+            Assert.AreEqual(200, result2.StatusCode);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    [Test]
+    public void Get_UnknownGuidPassed_ReturnsNotFoundResult()
+    {
+        try
+        {
+            
+            Assert.AreEqual(404, result2.StatusCode);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }*/
+
+
 }
